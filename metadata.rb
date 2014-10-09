ba478431cd42fe91377eafe3c2934b20b5c38e82
Trevor Bramwell <trevor@bramwell.net>
2014-10-09 16:23:15 -0700

    Initial Commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'osl-jenkins'
+maintainer       'Oregon State University'
+maintainer_email 'systems@osuosl.org'
+license          'Apache 2.0'
+description      'Installs/Configures osl-jenkins'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
