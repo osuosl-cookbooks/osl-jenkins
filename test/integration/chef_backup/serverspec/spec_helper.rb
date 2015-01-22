2064ce68e0ca896d2293431a3c793c8b21ebc096
Trevor Bramwell <trevor@bramwell.net>
2015-01-22 11:46:57 -0800

    Fix rubocop 80 char limit in spec_helper

--- test/integration/chef_backup/serverspec/spec_helper.rb
+++ test/integration/chef_backup/serverspec/spec_helper.rb
@@ -4,6 +4,7 @@ set :backend, :exec
 
 RSpec.configure do |config|
   config.before(:all) do
-    config.path = '/opt/chef/embedded/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+    config.path = '/opt/chef/embedded/bin:/usr/local/sbin:
+                   /usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
   end
 end
