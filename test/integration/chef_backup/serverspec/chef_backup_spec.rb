46839964b43f7e8f73a2e47cda068dd8def4aebc
armiller <armiller@osuosl.org>
2015-02-18 10:47:52 -0800

    Merge into one file

--- test/integration/chef_backup/serverspec/chef_backup_spec.rb
+++ test/integration/chef_backup/serverspec/chef_backup_spec.rb
@@ -1,4 +1,13 @@
-require 'spec_helper'
+require 'serverspec'
+
+set :backend, :exec
+
+RSpec.configure do |config|
+  config.before(:all) do
+    config.path = '/opt/chef/embedded/bin:/usr/local/sbin:
+                   /usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+  end
+end
 
 describe package('knife-backup') do
   it { should be_installed.by('gem').with_version('0.0.10') }
