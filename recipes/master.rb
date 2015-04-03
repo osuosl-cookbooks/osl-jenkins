7d4291afac136b64501dff851d099df81265a274
Trevor Bramwell <trevor@bramwell.net>
2015-04-03 10:47:14 -0700

    Add master recipe for setting jenkins master attributes

---
+++ recipes/master.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook Name:: osl-jenkins
+# Recipe:: master
+#
+# Copyright 2014, Oregon State University
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+node.default['jenkins']['master']['install_method'] = 'war'
+node.default['jenkins']['master']['version'] = '1.555'
+node.default['jenkins']['master']['checksum'] = '31f5c2a3f7e843f7051253d640f07f7c24df5e9ec271de21e92dac0d7ca19431'
+node.default['jenkins']['master']['listen_address'] = '127.0.0.1'
