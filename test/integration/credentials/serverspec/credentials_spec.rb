a0a70cd15d63d33d176b752ae77a206e0f32089e
Trevor Bramwell <trevor@bramwell.net>
2015-04-13 10:55:10 -0700

    Add tests for credentials recipe
    - Use chef-zero provider because of data-bags.
    - Adds encrypted test data-bag: ssh-keys
    - Ensures public ssh-key in ssh-keys data bag assigned to jenkins
      user.

---
+++ test/integration/credentials/serverspec/credentials_spec.rb
@@ -0,0 +1,15 @@
+require 'serverspec'
+
+set :backend, :exec
+
+describe file('/var/lib/jenkins/users/jenkins/config.xml') do
+  it { should be_file }
+  it do
+    should contain 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC2aaJit5+siL2HWkho' \
+    'ObF/FHLlj+vQjIORq0VADAhzcLCywRA6cab68dobw31W/wGRbEhuhm9YxZRV+nncOkRykRX' \
+    '3QDbRUeUSqGPFyE487OgPK9LaPve1Tu+gug0TDrOtEgXlFnzhKrQesglpF8R+cUmIYsJX2i' \
+    'sgbtNdEUpFunl/Dyjo2iXSVkr1ZRjr99do89EwvUB2XflZYoPxnqT+uufMn2WxJM5PpqEkD' \
+    'mdwVx0q2LxYvksxgsKcCdEvKBYgt0+sBpqFxLuCHMwgQPDPDh2rcMhBfu1Budc8Af8LGwAA' \
+    '48gehZyC29ZkoY75QNgcPtocrg/2VVLNRX3qGjN9 jenkins@osuosl'
+  end
+end
