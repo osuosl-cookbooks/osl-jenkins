d72b329d97e354d56cd3033d7b61d0be9b832703
Trevor Bramwell <trevor@bramwell.net>
2015-04-24 10:47:01 -0700

    Add serverspec test for chef_ci_cookbook_template

---
+++ test/integration/chef_ci_cookbook_template/serverspec/chef_ci_cookbook_template_spec.rb
@@ -0,0 +1,18 @@
+require 'serverspec'
+
+set :backend, :exec
+
+RSpec.configure do |config|
+  config.before(:all) do
+    config.path = '/opt/chef/embedded/bin:/usr/local/sbin:
+                   /usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+  end
+end
+
+describe package('foodcritic') do
+  it { should be_installed.by('gem').with_version('4.0.0') }
+end
+
+describe package('rubocop') do
+  it { should be_installed.by('gem').with_version('0.27.1') }
+end
