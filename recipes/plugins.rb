94da8c1059cfc7bc2c4525d26b94de83b8bd24c0
Sean Rettig <seanrettig@gmail.com>
2015-05-04 12:14:00 -0700

    move plugin installation to separate recipe, use attributes

--- recipes/plugins.rb
+++ recipes/plugins.rb
@@ -17,5 +17,8 @@
 # limitations under the License.
 #
 
-node.default['jenkins']['master']['version'] = '1.608-1.1'
-node.default['jenkins']['master']['listen_address'] = '127.0.0.1'
+node['osl-jenkins']['plugins'].each do |plugin|
+  jenkins_plugin plugin do
+    notifies :restart, 'service[jenkins]'
+  end
+end
