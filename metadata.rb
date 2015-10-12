1a608f81bc3fca7893152543a205760017e34b9c
Jordan Evans <jordane@osuosl.org>
2015-10-12 13:24:17 -0700

    alphabetize deps

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,9 @@ description      'Installs/Configures osl-jenkins'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.5.1'
 
-depends          'jenkins'
-depends          'osl-haproxy'
 depends          'chef-dk'
 depends          'java'
+depends          'jenkins'
+depends          'osl-haproxy'
 depends          'ssh-keys'
 depends          'sudo'
