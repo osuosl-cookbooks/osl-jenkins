ddade8b0d5735427df8e4d2fb524c87678f301e1
Sean Rettig <seanrettig@gmail.com>
2015-12-28 16:16:08 -0800

    add org attribute and actually pass org/repo to template

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,2 @@
+default['osl-jenkins']['org'] = 'osuosl-cookbooks'
 default['osl-jenkins']['databag'] = 'osl_jenkins'
--- recipes/cookbook_uploader.rb
+++ recipes/cookbook_uploader.rb
@@ -17,17 +17,22 @@
 # limitations under the License.
 #
 
+org = node['osl-jenkins']['org']
 secrets = Chef::EncryptedDataBagItem.load(node['osl-jenkins']['databag'],
                                           'secrets')
-repos = collect_github_repositories(secrets, 'osuosl-cookbooks')
+repos = collect_github_repositories(secrets, org)
 repos.each do |repo|
-  xml = ::File.join(Chef::Config[:file_cache_path], repo, 'config.xml')
+  xml = ::File.join(Chef::Config[:file_cache_path], org, repo, 'config.xml')
   d = ::File.dirname(xml)
   directory d do
     recursive true
   end
   template xml do
     source 'cookbook-uploader.config.xml.erb'
+    variables(
+      org: org,
+      repo: repo
+    )
   end
   jenkins_job repo do
     config xml
