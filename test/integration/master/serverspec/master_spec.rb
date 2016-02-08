d9aee1085f566e6fd902737d317e6fe484bd4e62
armiller <armiller@osuosl.org>
2016-02-08 11:11:25 -0800

    Update jenkins version test and exclude jenkins in yum-cron

--- test/integration/master/serverspec/master_spec.rb
+++ test/integration/master/serverspec/master_spec.rb
@@ -3,7 +3,7 @@ require 'serverspec'
 set :backend, :exec
 
 describe package('jenkins') do
-  it { should be_installed.with_version('1.608-1.1') }
+  it { should be_installed.with_version('1.643-1.1') }
 end
 
 describe port('8080') do
