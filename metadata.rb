5eb11491122792357b920298f3271341d0aa69f4
armiller <armiller@osuosl.org>
2016-02-15 18:12:07 -0800

    Bump cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'systems@osuosl.org'
 license          'Apache 2.0'
 description      'Installs/Configures osl-jenkins'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.5.4'
+version          '0.5.5'
 
 depends          'chef-dk'
 depends          'java'
