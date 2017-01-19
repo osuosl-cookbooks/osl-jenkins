3dfded7794b6f42126493693b8383c24858037fa
Lance Albertson <lance@osuosl.org>
2017-01-18 17:10:06 -0800

    Refactored code that makes rubocop happy

--- files/default/dns_zone_serial_bump.rb
+++ files/default/dns_zone_serial_bump.rb
@@ -6,48 +6,67 @@ require 'git'
 require 'json'
 require 'octokit'
 
-$commit_msg = "\n\n"
+# Library to bump DNS zones
+class BumpZone
+  @commit_msg = "\n\n"
+  @soa_oneline = nil
+  ONELINE_REGEX = /(^[\w\d\.-_]+\s+[\d]+\s+IN\s+SOA.*[\w\d\.-_]+\s+)(\d{10})(\s+[\d]+\s+[\d]+\s+[\d]+\s+[\d]+)/
+  MULTILINE_REGEX = /(^[\w\d\.-_]+\s+IN\s+SOA\s+[\w\d\.-_]+\s+[\w\d\.-_]+\s+\(\n\s+)(\d{10})(.*\n.*\n.*\n.*\n.*\n\s+\))/
 
-def _regex(file, regex)
-  !!(file =~ regex)
-end
-
-def bump_zone(zone_file)
-  soa_oneline_regex = /(^[\w\d\.-_]+\s+[\d]+\s+IN\s+SOA.*[\w\d\.-_]+)\s+(\d{10})\s+([\d]+\s+[\d]+\s+[\d]+\s+[\d]+)/
-  soa_multiline_regex = /(^[\w\d\.-_]+\s+IN\s+SOA\s+[\w\d\.-_]+\s+[\w\d\.-_]+\s+\(\n\s+)(\d{10})(.*\n.*\n.*\n.*\n.*\n\s+\))/
-  new_serial = nil
-  zone = ::File.read(zone_file)
-  if _regex(zone, soa_oneline_regex)
-    soa_oneline = true
-  elsif _regex(zone, soa_multiline_regex)
-    soa_oneline = false
-  else
-    puts 'no soa found!'
-    exit 1
+  class << self
+    attr_reader :commit_msg
   end
-  zone_new = zone.gsub(soa_oneline ? soa_oneline_regex : soa_multiline_regex) do
-    before_serial = Regexp.last_match(1)
-    cur_serial = Regexp.last_match(2).to_i
-    after_serial = Regexp.last_match(3)
-    new_serial = Time.new.strftime('%Y%m%d00').to_i
-    counter = cur_serial.to_s.match(/\d{2}$/)
-    if counter.to_s =~ /99/
-      puts 'error!'
+
+  def self.add_commit_msg(msg)
+    @commit_msg << msg
+  end
+
+  def self.regex(file, regex)
+    !(file !~ regex)
+  end
+
+  def self.zone_type(zone)
+    if regex(zone, ONELINE_REGEX)
+      @soa_oneline = true
+    elsif regex(zone, MULTILINE_REGEX)
+      @soa_oneline = false
+    else
+      puts 'no soa found!'
       exit 1
     end
-    new_serial = 1 + cur_serial if new_serial <= cur_serial
-    $commit_msg << "Bump #{zone_file} from #{cur_serial} to #{new_serial}\n"
-    if soa_oneline
-      "#{before_serial} #{new_serial} #{after_serial}"
-    else
-      "#{before_serial}#{new_serial}#{after_serial}"
-    end
   end
 
-  ::File.write(zone_file, zone_new)
+  def self.print_soa(before_serial, serial, after_serial)
+    "#{before_serial}#{serial}#{after_serial}"
+  end
+
+  def self.counter(cur_serial)
+    counter = cur_serial.to_s.match(/\d{2}$/)
+    puts 'error!' if counter.to_s =~ /99/
+  end
+
+  def self.new_serial(zone_file, filename)
+    cur_serial = zone_file.match(@soa_oneline ? ONELINE_REGEX : MULTILINE_REGEX)[2].to_i
+    new_serial = Time.new.strftime('%Y%m%d00').to_i
+    BumpZone.counter(cur_serial)
+    new_serial = 1 + cur_serial.to_i if new_serial <= cur_serial.to_i
+    BumpZone.add_commit_msg("Bump #{filename} from #{cur_serial} to #{new_serial}\n")
+    new_serial.to_i
+  end
+
+  def self.bump(zone_file)
+    zone = ::File.read(zone_file)
+    BumpZone.zone_type(zone)
+    new_serial = BumpZone.new_serial(zone, zone_file)
+    zone_new = zone.gsub(@soa_oneline ? ONELINE_REGEX : MULTILINE_REGEX) do
+      BumpZone.print_soa(Regexp.last_match(1), new_serial, Regexp.last_match(3))
+    end
+
+    ::File.write(zone_file, zone_new)
+  end
 end
 
-d = JSON.load(STDIN.read)
+d = JSON.parse(STDIN.read)
 
 unless d['action'] == 'closed' && d['pull_request']['merged'] == true
   puts 'This isn\'t a merged request, so don\'t do anything'
@@ -56,13 +75,12 @@ end
 
 # Get PR object
 github = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
-repo_name = d['repository']['name']
 repo_path = d['repository']['full_name']
 issue_number = d['number']
 
 files = github.pull_request_files(repo_path, issue_number)
 files.each do |f|
-  bump_zone(f.filename) if f.filename =~ /^db\..*/
+  BumpZone.bump(f.filename) if f.filename =~ /^db\..*/
 end
 
 # Set up the git gem
@@ -70,7 +88,7 @@ git = Git.open('.')
 
 # Commit changes
 git.add(all: true)
-git.commit("Automatically bumping zone serials#{$commit_msg}")
+git.commit("Automatically bumping zone serials#{BumpZone.commit_msg}")
 # Push back to Github
 # git.push(git.remote('origin'), 'master')
 
