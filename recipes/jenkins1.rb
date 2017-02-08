016bf2782708eb07c10891ba6a2e61a6092d373b
Lance Albertson <lance@osuosl.org>
2017-02-08 15:13:59 -0800

    Import attributes from node itself
    I noticed these were manually in the node itself, so let's put them here so we
    can track them.

--- recipes/jenkins1.rb
+++ recipes/jenkins1.rb
@@ -15,6 +15,19 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+node.default['osl-jenkins']['cookbook_uploader'].tap do |conf|
+  conf['authorized_teams'] = %w(osuosl-cookbooks/staff)
+  conf['chef_repo'] = 'osuosl/chef-repo'
+  conf['default_environments'] = %w(
+    production
+    workstation
+    openstack_production
+    openstack_staging
+    phpbb
+  )
+  conf['org'] = 'osuosl-cookbooks'
+end
+
 include_recipe 'osl-jenkins::master'
 include_recipe 'osl-jenkins::chef_ci_cookbook_template'
 include_recipe 'osl-jenkins::osl_cookbook_uploader'
