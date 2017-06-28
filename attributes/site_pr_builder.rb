a47f487fd92ce4073dfcaf1e22a284c3e7e9378d
Ken Lett <kennric@osuosl.org>
2017-06-28 15:19:40 -0700

    separate github orgs

--- attributes/site_pr_builder.rb
+++ attributes/site_pr_builder.rb
@@ -2,11 +2,11 @@ default['osl-jenkins']['site_pr_builder'] = {
   'bin_path' => ::File.join(node['jenkins']['master']['home'], 'bin'),
   'lib_path' => ::File.join(node['jenkins']['master']['home'], 'lib'),
   'sites_to_build' => {
-    'beaver-barcamp-pelican' => 'https://github.com/osuosl/beaver-barcamp-pelican.git',
-    'cass-pelican' => 'https://github.com/osu-cass/cass-pelican.git',
-    'osuosl-pelican' => 'https://github.com/osuosl/osuosl-pelican.git',
-    'wiki' => 'https://github.com/osuosl/wiki.git',
-    'docs' => 'https://github.com/osuosl/docs.git'
+    'beaver-barcamp-pelican' => 'osuosl',
+    'cass-pelican' => 'osu-cass',
+    'osuosl-pelican' => 'osuosl',
+    'wiki' => 'osuosl',
+    'docs' => 'osuosl'
   },
   'credentials' => {
     'trigger_token' => nil,
