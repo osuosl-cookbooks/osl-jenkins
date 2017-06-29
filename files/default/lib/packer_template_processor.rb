d2594059f1de114df94e487866fd727701ba3ba8
Samarendra M H <samarendra@osuosl.org>
2017-06-28 18:01:41 -0700

    rubocop fixes and a stub function for processing github payload

--- files/default/lib/packer_template_processor.rb
+++ files/default/lib/packer_template_processor.rb
@@ -6,39 +6,34 @@ require 'pry'
 # Finds what templates are composed of a given script
 # @param filename
 #
-# This is useful to figure out what images should be rebuilt when a 
+# This is useful to figure out what images should be rebuilt when a
 # composing script is modified
 #
 # @todo implement a good deep search algorithm not depending on the structure
 def find_dependent_templates(composing_script)
-  begin
-    dependent_templates = []
-    
-    # go to dir containing all images
-    Dir.chdir(@packer_templates_dir)
+  dependent_templates = []
 
-    # iterate through images and look whether they refer to composing_script
-    Dir.glob('*.json') do |t|
-      t_data = JSON.load(open(t))
-      if t_data.include? 'provisioners' and not t_data['provisioners'].empty?
-        if t_data['provisioners'][0].include? 'scripts'
-          dependent_templates << t if t_data['provisioners'][0]['scripts'].include? composing_script
-        end
+  # go to dir containing all images
+  Dir.chdir(@packer_templates_dir)
+
+  # iterate through images and look whether they refer to composing_script
+  Dir.glob('*.json') do |t|
+    t_data = JSON.parse(open(t))
+    if (t_data.include? 'provisioners') && t_data['provisioners'].any?
+      if t_data['provisioners'][0].include? 'scripts'
+        dependent_templates << t if t_data['provisioners'][0]['scripts'].include? composing_script
       end
     end
-  rescue Exception => e
-    puts e
   end
-  return dependent_templates
+  dependent_templates
 end
 
 # Takes github payload and extracts filenames from it
 # to further process
 
 def github_payload_processor(payload)
-  # if template files in payload
-    # add them to the openstack_taster queue
+  # if template files in payload, add them to the openstack_taster queue
   # if scripts in the payload
-    # find the dependent templates (apart from the ones already added)
-    # add them to the openstack_taster queue
+  # find the dependent templates (apart from the ones already added)
+  # add them to the openstack_taster queue
 end
