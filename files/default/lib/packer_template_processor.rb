902fc29d9bcc7b5da5da8e4d825caa091951230c
Samarendra M H <samarendra@osuosl.org>
2017-06-28 17:47:27 -0700

    ruby things to handle the needs of bento pipelines

---
+++ files/default/lib/packer_template_processor.rb
@@ -0,0 +1,33 @@
+require 'json'
+require 'pry'
+
+@packer_templates_dir = ENV['PACKER_TEMPLATES_DIR'] || './bento/packer'
+
+# Finds what templates are composed of a given script
+# @param filename
+#
+# This is useful to figure out what images should be rebuilt when a 
+# composing script is modified
+#
+# @todo implement a good deep search algorithm not depending on the structure
+def find_dependent_templates(composing_script)
+  begin
+    dependent_templates = []
+    
+    # go to dir containing all images
+    Dir.chdir(@packer_templates_dir)
+
+    # iterate through images and look whether they refer to composing_script
+    Dir.glob('*.json') do |t|
+      t_data = JSON.load(open(t))
+      if t_data.include? 'provisioners' and not t_data['provisioners'].empty?
+        if t_data['provisioners'][0].include? 'scripts'
+          dependent_templates << t if t_data['provisioners'][0]['scripts'].include? composing_script
+        end
+      end
+    end
+  rescue Exception => e
+    puts e
+  end
+  return dependent_templates
+end
