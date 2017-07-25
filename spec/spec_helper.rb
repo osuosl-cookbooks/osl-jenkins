1662e66c8ce0b00e7c3d7f9f044dce69337a691c
Samarendra M H <samarendra@osuosl.org>
2017-07-25 16:13:41 -0700

    unit tests for packer_pipeline_node recipe

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,6 +36,26 @@ shared_context 'common_stubs' do
   end
 end
 
+shared_context 'data_bag_stubs' do
+  before do
+    stub_data_bag_item('users','alfred').and_return(
+      'id': 'alfred',
+      'ssh_keys': [
+        'test_ssh_key'
+      ]
+    )
+
+    allow(Chef::EncryptedDataBagItem).to receive(:load)
+      .with('osl_jenkins','packer_pipeline_creds')
+      .and_return(
+        'id': 'packer_pipeline_creds'
+      )
+      .and_raise(Net::HTTPServerException.new(
+                   'osl_jenkins databag not found',
+                   Net::HTTPResponse.new('1.1', '404', '')
+      ))
+  end
+end
 shared_context 'cookbook_uploader' do
   before do
     allow_any_instance_of(Chef::Recipe).to receive(:set_up_github_push)
