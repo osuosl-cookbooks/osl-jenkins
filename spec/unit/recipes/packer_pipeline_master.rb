bd3598ac86c3df6aafda0d3103ed4d95c12a95cf
Samarendra M H <samarendra@osuosl.org>
2017-08-04 16:07:15 -0700

    ChefSpec

---
+++ spec/unit/recipes/packer_pipeline_master.rb
@@ -0,0 +1,69 @@
+require_relative '../../spec_helper.rb'
+
+describe 'osl-jenkins::packer_pipeline_master' do
+  ALL_PLATFORMS.each do |p|
+    context "#{p[:platform]} #{p[:version]}" do
+      cached(:chef_run) do
+        ChefSpec::SoloRunner.new(p) do |node|
+          node.set['osl-jenkins']['credentials']['jenkins'] = {
+            'packer_pipeline' => {
+              user: 'manatee',
+              api_token: 'token_password',
+              trigger_token: 'trigger_token'
+            }
+          }
+        end.converge(described_recipe)
+      end
+      include_context 'common_stubs'
+      it 'converges successfully' do
+        expect { chef_run }.to_not raise_error
+      end
+      %w(/var/lib/jenkins/bin /var/lib/jenkins/lib).each do |d|
+        it do
+          expect(chef_run).to create_directory(d).with(recursive: true)
+        end
+      end
+      it do
+        expect(chef_run).to create_cookbook_file('/var/lib/jenkins/bin/packer_pipeline.rb')
+          .with(
+            source: 'bin/packer_pipeline.rb',
+            owner: 'jenkins',
+            group: 'jenkins',
+            mode: 0550
+          )
+      end
+      it do
+        expect(chef_run).to create_cookbook_file('/var/lib/jenkins/lib/packer_pipeline.rb')
+          .with(
+            source: 'lib/packer_pipeline.rb',
+            owner: 'jenkins',
+            group: 'jenkins',
+            mode: 0440
+          )
+      end
+      %w(git octokit faraday-http-cache).each do |g|
+        it do
+          expect(chef_run).to install_chef_gem(g).with(compile_time: true)
+        end
+      end
+
+      it 'creates the directory' do
+        expect(chef_run).to create_directory('/var/chef/cache/packer_pipeline').with(recursive: true)
+      end
+
+      it 'creates the packer_pipeline jenkins job' do
+        expect(chef_run).to create_jenkins_job('packer_pipeline').with(
+          config: '/var/chef/cache/packer_pipeline/config.xml'
+        )
+      end
+
+      it 'creates the github pr job config file' do
+        expect(chef_run).to create_template('/var/chef/cache/packer_pipeline/config.xml')
+          .with(
+            source: 'packer_pipeline.config.xml.erb',
+            mode: 0440
+          )
+      end
+    end
+  end
+end
