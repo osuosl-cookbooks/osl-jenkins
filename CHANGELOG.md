aca26b21ac672781b8b32a9821985b084e4f418e
Manatee <github@osuosl.org>
2017-08-07 17:16:52 +0000

    Automatic patch-level version bump to v1.8.3 by Jenkins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ osl-jenkins CHANGELOG
 This file is used to list changes made in each version of the
 osl-jenkins cookbook.
 
+1.8.3 (2017-08-07)
+------------------
+- the real true packer pipeline job
+
 1.8.2 (2017-08-03)
 ------------------
 - setup keys to use with openstack taster
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'systems@osuosl.org'
 license          'Apache 2.0'
 description      'Installs/Configures osl-jenkins'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.8.2'
+version          '1.8.3'
 issues_url       'https://github.com/osuosl-cookbooks/osl-jenkins/issues'
 source_url       'https://github.com/osuosl-cookbooks/osl-jenkins'
 
