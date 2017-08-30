5d53ed4641ff0cd9e2726c1c188c0d8213880dc3
Samarendra M H <samarendra@osuosl.org>
2017-08-29 17:59:52 -0700

    Set status on PR for packer-templates repo
    This also changes the original packer_pipeline ruby binary to
    accept a JSON file for either processing the GitHub payload or to
    set the status by processing the final results array crated by the
    pipeline

--- files/default/bin/packer_pipeline.rb
+++ files/default/bin/packer_pipeline.rb
@@ -1,4 +1,66 @@
 #!/opt/chef/embedded/bin/ruby
 require_relative '../lib/packer_pipeline'
+require 'json'
+require 'optparse'
 
-puts PackerPipeline.start.to_json
+options = {}
+
+parser = OptionParser.new do |opts|
+  opts.banner = """
+        Usage: #{$0} -p PAYLOAD_JSON_FILE -f FINAL_RESULTS_FILE
+        You can either pass the PAYLOAD_JSON_FILE to process and begin the pipeline processing
+        OR the FINAL_RESULTS_FILE to send back to GitHub
+  """
+
+  opts.separator('')
+  opts.on('-p PAYLOAD_JSON_FILE',
+          '--payload_file PAYLOAD_JSON_FILE',
+          'Specify the JSON payload to process.') do |p|
+    options[:payload_json_file] = p
+  end
+
+  opts.on('-f FINAL_RESULTS_FILE',
+          '--final_results_file FINAL_RESULTS_FILE',
+          'Specify the JSON file containing the final results of pipeline processing.') do |f|
+    options[:final_results_file] = f
+  end
+
+  opts.on_tail('-h', '--help', 'Prints this help text') do
+    puts opts
+    exit
+  end
+end
+
+parser.parse! ARGV
+
+puts options
+
+if !options.key?(:payload_json_file) &&
+   !options.key?(:final_results_file)
+
+  puts "Either you must pass the PAYLOAD_JSON_FILE or the FINAL_RESULTS_FILE!"
+  exit 1
+end
+
+
+if options[:final_results_file]
+  if File.readable? options[:final_results_file]
+    final_results = JSON.parse(File.read(options[:final_results_file]))
+    PackerPipeline.new().set_commit_status(final_results)
+    exit 0
+  else
+    puts "Final results file #{options[:final_results_file]} is not readable!"
+    exit 2
+  end
+end
+
+if options[:payload_json_file]
+  if File.readable? options[:payload_json_file]
+    payload = JSON.parse(File.read(options[:payload_json_file]))
+    PackerPipeline.new().process_payload(payload).to_json
+    exit 0
+  else
+    puts "Payload JSON file #{options[:payload_json_file]} is not readable!"
+    exit 2
+  end
+end
--- files/default/lib/packer_pipeline.rb
+++ files/default/lib/packer_pipeline.rb
@@ -13,12 +13,17 @@ Octokit.middleware = stack
 
 # Library to process github payload for Packer Template Pipeline
 class PackerPipeline
+
+  def self.new
+    @github = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'], auto_paginate: true)
+    @repo_path = 'osuosl/packer-templates'
+    self
+  end
+
   # find the changed files associated with the PR
   def self.changed_files(json)
-    github = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'], auto_paginate: true)
-    repo_path = json['repository']['full_name']
     issue_number = json['number']
-    github.pull_request_files(repo_path, issue_number)
+    @github.pull_request_files(@repo_path, issue_number)
   end
 
   # If file is a template, return it in an array.
@@ -61,15 +66,66 @@ class PackerPipeline
     dependent_templates
   end
 
-  def self.start
-    d = JSON.parse(STDIN.read)
+  # update status for the PR from the final_results given by the pipeline
+  # the final_results is a JSON hash containing all the templates that were processed.
+  def self.set_commit_status(final_results)
+    git_commit = ENV['GIT_COMMIT']
+    return unless git_commit
+
+    # we are processing a JSON array which looks liek
+    # {
+    #   'arch': {
+    #       'template_name' : {
+    #             'linter' : 0,
+    #             'builder': 0,
+    #             'deploy_test' : 0,
+    #             'taster' : 0,
+    #         }
+    #   }
+    # }
+
+    #TODO: make this point to the specific job's console output
+    target_url = 'https://jenkins.osuosl.org/job/packer_pipeline'
+
+    p final_results 
+
+    final_status = {}
+
+    final_results.keys.each do |arch|
+      final_results[arch].keys.each do |t|
+        #create a context array per template
+        final_status[t] = { options: {
+            context: t,
+            target_url: target_url,
+          }
+        }
+
+        final_results[arch][t].keys.each do |stage|
+          if final_results[arch][t][stage] != 0
+            final_status[t][:state] = 'failure'
+            final_status[t][:options][:description] = "#{stage} failed!"
+            break
+          end
+
+          final_status[t][:state] = 'success'
+          final_status[t][:options][:description] = "All passed! #{final_results[arch][t]}"
+        end
+      #set status
+      @github.create_status(@repo_path, git_commit, final_status[t][:state], final_status[t][:options])
+      end
+    end
+
+    puts "We set result as #{final_status}"
+  end
+
+  def self.process_payload(payload)
 
     # Iterate through all the changed files and get an array of affected templates.
     # find_dependent_templates always returns an array of strings, so I use
     # .reduce to stitch those arrays together by doing the + on arrays of dependent
     # templates for each script. Finally .uniq will ensure we don't get the same
     # template twice in the array
-    templates = PackerPipeline.changed_files(d).map do |f|
+    templates = PackerPipeline.changed_files(payload).map do |f|
       find_dependent_templates(f.filename)
     end.reduce(:+).uniq
 
