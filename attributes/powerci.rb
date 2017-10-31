5b71d14f03f38efab0a8db912e0e763701bd6bc7
Lance Albertson <lance@osuosl.org>
2017-10-31 14:07:04 -0700

    Add CUDA images to POWERCI

--- attributes/powerci.rb
+++ attributes/powerci.rb
@@ -1,8 +1,12 @@
 default['osl-jenkins']['powerci']['docker_images'] = %w(
   osuosl/ubuntu-ppc64le:16.04
+  osuosl/ubuntu-ppc64le-cuda:8.0
+  osuosl/ubuntu-ppc64le-cuda:8.0-cudnn6
   osuosl/debian-ppc64le:9
   osuosl/fedora-ppc64le:26
   osuosl/centos-ppc64le:7
+  osuosl/centos-ppc64le-cuda:8.0
+  osuosl/centos-ppc64le-cuda:8.0-cudnn6
 )
 default['osl-jenkins']['powerci']['docker']['memory_limit'] = 'null'
 default['osl-jenkins']['powerci']['docker']['memory_swap'] = 'null'
