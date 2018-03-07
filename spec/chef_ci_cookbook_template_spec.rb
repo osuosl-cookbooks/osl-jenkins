a55dc9809a28a15297c53697810454843ef7b7c7
eldebrim <eldebrim@osuosl.org>
2018-03-07 14:31:49 -0800

    Stub netaddr version so chefspec tests pass

--- spec/chef_ci_cookbook_template_spec.rb
+++ spec/chef_ci_cookbook_template_spec.rb
@@ -6,6 +6,7 @@ describe 'osl-jenkins::chef_ci_cookbook_template' do
       cached(:chef_run) do
         ChefSpec::SoloRunner.new(p).converge(described_recipe)
       end
+      include_context 'common_stubs'
       before do
         stub_command('/opt/chefdk/embedded/bin/rubocop --version | grep 0.42.0')
       end
