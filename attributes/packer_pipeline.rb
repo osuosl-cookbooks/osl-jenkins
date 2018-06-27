714b0f61b8b91657400a4ac772c76872a0a63ca0
Lance Albertson <lance@osuosl.org>
2018-06-27 09:17:41 -0700

    Always upgrade openstack_taster
    Since we control when new releases of openstack_taster, we don't need to lock
    the version in the cookbook. This removes the requirement of bumping this
    cookbook on every new release. It also ensures that it always uses the latest
    version which it currently does even if you bump the version.

--- attributes/packer_pipeline.rb
+++ attributes/packer_pipeline.rb
@@ -1,4 +1,3 @@
-default['osl-jenkins']['packer_pipeline']['openstack_taster_version'] = '1.0.2'
 default['osl-jenkins']['packer_pipeline'] = {
   'bin_path' => ::File.join(node['jenkins']['master']['home'], 'bin'),
   'lib_path' => ::File.join(node['jenkins']['master']['home'], 'lib'),
