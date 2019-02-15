ac0203a8efbb04fa13642aac6ae4073fd9f4678b
jerrypeng0112 <jerrypeng0112@hotmail.com>
2019-02-15 08:13:13 -0800

    Added a method to read yml file, finished the first test for loading from yml fixture file

--- spec/webhooks/bump_environment_spec.rb
+++ spec/webhooks/bump_environment_spec.rb
@@ -18,6 +18,10 @@ module SpecHelper
     JSON.parse(open_fixture(name))
   end
 
+  def open_yaml(name)
+    YAML.load_file(fixture_path(name))
+  end
+
   def tempfile(file)
     tempfile = Tempfile.new("#{Pathname.new(file).basename}-rspec")
     tempfile.write(::File.read(file))
@@ -32,23 +36,23 @@ end
 
 describe BumpEnvironments do
   let(:github_mock) { double('Octokit', commits: [], issues: [], same_options?: false, auto_paginate: true) }
-  let(:yaml_mock) { YAML.load_file(open_fixture('bump_environmnets.yml')) }
   context 'load_node_attr' do
     it 'Load node attributes' do
-      allow(YAML).to receive(load_file).with('bump_environments.yml')
-          .and_return(open_fixture)
+      allow(YAML).to receive(:load_file).with('bump_environments.yml')
+          .and_return(open_yaml('bump_environments.yml'))
+      BumpEnvironments.load_node_attr
       expect(BumpEnvironments.default_chef_envs).to contain_exactly(
         'openstack_mitaka', 'phase_out_nginx', 'phpbb', 'production', 'testing', 'workstation'
       )
-      expect(BumpEnvironments.default_environmnets_word).to match(/~/)
-      expect(BumpEnvironments.all_environmnets_word).to match(/\*/)
+      expect(BumpEnvironments.default_chef_envs_word).to match(/~/)
+      expect(BumpEnvironments.all_chef_envs_word).to match(/\*/)
       expect(BumpEnvironments.chef_repo).to match(/osuosl\/chef-repo/)
-      expect(BumpEnvironments.chef_repo).to match(/github_token/)
+      expect(BumpEnvironments.github_token).to match(/github_token/)
     end
   end
-  it 'verify chef env' do
-    BumpEnvironments.verify_chef_env
-    expect(self).to receive(:verify_default_chef_env)
-    expect(self).to receive(:verify_all_chef_env)
-  end
+#  it 'verify chef env' do
+#    BumpEnvironments.verify_chef_env
+#    expect(self).to receive(:verify_default_chef_env)
+#    expect(self).to receive(:verify_all_chef_env)
+#  end
 end
