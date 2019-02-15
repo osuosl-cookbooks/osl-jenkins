6771e774cd9067a0545ccf9e59b73c177746d4cb
jerrypeng0112 <jerrypeng0112@hotmail.com>
2019-02-14 16:48:34 -0800

    Added specs file that does not work

--- spec/webhooks/bump_environment_spec.rb
+++ spec/webhooks/bump_environment_spec.rb
@@ -2,6 +2,7 @@ require 'rspec'
 require 'rspec/mocks'
 require 'tempfile'
 require 'pathname'
+require 'yaml'
 require_relative '../../files/default/lib/bump_environments'
 
 module SpecHelper
@@ -30,13 +31,24 @@ RSpec.configure do |c|
 end
 
 describe BumpEnvironments do
-  it 'verify chef env'
+  let(:github_mock) { double('Octokit', commits: [], issues: [], same_options?: false, auto_paginate: true) }
+  let(:yaml_mock) { YAML.load_file(open_fixture('bump_environmnets.yml')) }
+  context 'load_node_attr' do
+    it 'Load node attributes' do
+      allow(YAML).to receive(load_file).with('bump_environments.yml')
+          .and_return(open_fixture)
+      expect(BumpEnvironments.default_chef_envs).to contain_exactly(
+        'openstack_mitaka', 'phase_out_nginx', 'phpbb', 'production', 'testing', 'workstation'
+      )
+      expect(BumpEnvironments.default_environmnets_word).to match(/~/)
+      expect(BumpEnvironments.all_environmnets_word).to match(/\*/)
+      expect(BumpEnvironments.chef_repo).to match(/osuosl\/chef-repo/)
+      expect(BumpEnvironments.chef_repo).to match(/github_token/)
+    end
+  end
+  it 'verify chef env' do
     BumpEnvironments.verify_chef_env
     expect(self).to receive(:verify_default_chef_env)
     expect(self).to receive(:verify_all_chef_env)
-  end
-
-  context 'include default chef env word'
-     
   end
 end
