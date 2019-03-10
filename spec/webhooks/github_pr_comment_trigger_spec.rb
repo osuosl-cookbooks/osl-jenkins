99804d508c69adb05620cbdacaacf9e3971d8082
jerrypeng0112 <jerrypeng0112@hotmail.com>
2019-03-09 17:36:30 -0800

    Finished verify_valid_request

--- spec/webhooks/github_pr_comment_trigger_spec.rb
+++ spec/webhooks/github_pr_comment_trigger_spec.rb
@@ -81,9 +81,9 @@ describe GithubPrCommentTrigger do
     end
     it 'does not have created action (bump patch)' do
       begin
-        modified_json_patch = open_json('bump_patch.json')
-        modified_json_patch['action'] = 'not_created'
-        expect{ GithubPrCommentTrigger.verify_comment_creation(modified_json_patch) }
+        modified_json = open_json('bump_patch.json')
+        modified_json['action'] = 'not_created'
+        expect{ GithubPrCommentTrigger.verify_comment_creation(modified_json) }
           .to output('Exiting because comment was not a bump request').to_stderr
       rescue SystemExit => e
         expect(e.status).to eq(0)
@@ -91,9 +91,9 @@ describe GithubPrCommentTrigger do
     end
     it 'does not have created action (bump patch)' do
       begin
-        modified_json_minor = open_json('bump_minor.json')
-        modified_json_minor['action'] = 'not_created'
-        expect{ GithubPrCommentTrigger.verify_comment_creation(modified_json_minor) }
+        modified_json = open_json('bump_minor.json')
+        modified_json['action'] = 'not_created'
+        expect{ GithubPrCommentTrigger.verify_comment_creation(modified_json) }
           .to output('Exiting because comment was not a bump request').to_stderr
       rescue SystemExit => e
         expect(e.status).to eq(0)
@@ -101,9 +101,9 @@ describe GithubPrCommentTrigger do
     end
     it 'does not have created action (bump patch)' do
       begin
-        modified_json_major = open_json('bump_major.json')
-        modified_json_major['action'] = 'not_created'
-        expect{ GithubPrCommentTrigger.verify_comment_creation(modified_json_major) }
+        modified_json = open_json('bump_major.json')
+        modified_json['action'] = 'not_created'
+        expect{ GithubPrCommentTrigger.verify_comment_creation(modified_json) }
           .to output('Exiting because comment was not a bump request').to_stderr
       rescue SystemExit => e
         expect(e.status).to eq(0)
@@ -113,24 +113,40 @@ describe GithubPrCommentTrigger do
 
   context '#verify_valid_request' do
     it 'comments bump major' do
-      expect{ GithubPrCommentTrigger.verify_comment_creation(open_json('bump_major.json')) } 
+      expect{ GithubPrCommentTrigger.verify_valid_request(open_json('bump_major.json')) } 
         .to_not output().to_stderr
-      GithubPrCommentTrigger.verify_comment_creation(open_json('bump_major.json'))
-      puts GithubPrCommentTrigger.level
       expect(GithubPrCommentTrigger.level).to eql('major')
       expect(GithubPrCommentTrigger.envs).to eql('*')
     end
     it 'comments bump minor' do
-      expect{ GithubPrCommentTrigger.verify_comment_creation(open_json('bump_minor.json')) } 
+      expect{ GithubPrCommentTrigger.verify_valid_request(open_json('bump_minor.json')) } 
         .to_not output().to_stderr
       expect(GithubPrCommentTrigger.level).to eql('minor')
       expect(GithubPrCommentTrigger.envs).to eql('*')
     end
     it 'comments bump patch' do
-      expect{ GithubPrCommentTrigger.verify_comment_creation(open_json('bump_patch.json')) } 
+      expect{ GithubPrCommentTrigger.verify_valid_request(open_json('bump_patch.json')) } 
         .to_not output().to_stderr
       expect(GithubPrCommentTrigger.level).to eql('patch')
       expect(GithubPrCommentTrigger.envs).to eql('*')
+    end
+    it 'does not match comment' do
+      begin
+        modified_json = open_json('bump_major.json')
+        modified_json['comment']['body'] = '!bump  major  *'
+        expect{ GithubPrCommentTrigger.verify_valid_request(modified_json) }
+          .to output('Exiting because comment was not a bump request').to_stderr
+      rescue SystemExit => e
+        expect(e.status).to eq(0)
+      end
+    end
+    it 'does not include env in comment' do
+      modified_json = open_json('bump_major.json')
+      modified_json['comment']['body'] = '!bump major'
+      expect{ GithubPrCommentTrigger.verify_valid_request(modified_json) } 
+        .to_not output().to_stderr
+      expect(GithubPrCommentTrigger.level).to eql('major')
+      expect(GithubPrCommentTrigger.envs).to be_nil
     end
   end
 end
