50765465198964ff905bce50670f2e4d6e64507d
Khuong Luu <6059347+khashf@users.noreply.github.com>
2019-04-25 15:24:09 -0700

    Remove test/smoke directory
    No longer neccessary

--- test/smoke/default/bumpzone.rb
+++
@@ -1,6 +0,0 @@
-# # encoding: utf-8
-
-# Inspec test for recipe osl-jenkins::bumpzone
-
-# The Inspec reference, with examples and extensive documentation, can be
-# found at http://inspec.io/docs/reference/resources/
