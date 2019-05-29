ae2ba6a5e27bee038c7ed72d432f293da90ef956
Khuong Luu <6059347+khashf@users.noreply.github.com>
2019-05-28 17:56:43 -0700

    Update symlink for bumpzone

--- test/integration/helpers/inspec/00spec_helper.rb
+++ test/integration/helpers/inspec/00spec_helper.rb
@@ -7,11 +7,11 @@ describe package('java-1.8.0-openjdk') do
 end
 
 describe package('jenkins') do
-  its('version') { should eq '2.150.2-1.1' }
+  its('version') { should eq '2.164.2-1.1' }
 end
 
 describe command('yum versionlock') do
-  its('stdout') { should match(/^0:jenkins-2.150.2-1.1.x86_64$/) }
+  its('stdout') { should match(/^0:jenkins-2.164.2-1.1.x86_64$/) }
 end
 
 %w(80 443 8080).each do |p|
@@ -20,23 +20,14 @@ end
   end
 end
 
-# describe command('curl -v http://localhost/ 2>&1') do
-#   its('stdout') { should match(%r{HTTP/1.1 302 Found}) }
-#   its('stdout') { should match(%r{Location: https://localhost/}) }
-# end
-
 describe http('http://localhost/',
               enable_remote_worker: true) do
   its('status') { should cmp 302 }
   its('headers.Location') { should eq 'https://localhost/' }
 end
 
-# describe command('curl -k https://localhost/about/') do
-#   its('stdout') { should match(/Jenkins 2.150.2/) }
-# end
-
 describe http('https://localhost/about/',
               enable_remote_worker: true,
               ssl_verify: false) do
-  its('body') { should match(/Jenkins 2.150.2/) }
+  its('body') { should match(/Jenkins 2.164.2/) }
 end
