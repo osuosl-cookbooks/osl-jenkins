25a15cc49bf7a76270b1dd6a6424d81f4c320400
Lance Albertson <lance@osuosl.org>
2019-07-11 21:15:12 -0700

    Convert to using users_manage resource
    This replaces using user, group resources and the ssh-keys cookbook. This change
    is needed due to issues using newer ChefSpec with the ssh-keys cookbook. Also,
    it's been in our long term plan to migrate away from the user cookbook to the
    users cookbook.
    
    This change will require a similar change in the production data bag for the
    alfred user.

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -12,23 +12,10 @@ describe 'osl-jenkins::default' do
         expect { chef_run }.to_not raise_error
       end
       it do
-        expect(chef_run).to create_group('alfred').with(gid: 10000)
+        expect(chef_run).to create_users_manage('alfred').with(group_id: 10000)
       end
       it do
-        expect(chef_run).to create_user('alfred').with(
-          manage_home: true,
-          uid: 10000,
-          gid: 10000,
-          system: true,
-          shell: '/bin/bash',
-          home: '/home/alfred'
-        )
-      end
-      it do
-        expect(chef_run.group('alfred')).to notify('ohai[jenkins_reload_passwd]').to(:reload).immediately
-      end
-      it do
-        expect(chef_run.user('alfred')).to notify('ohai[jenkins_reload_passwd]').to(:reload).immediately
+        expect(chef_run.users_manage('alfred')).to notify('ohai[jenkins_reload_passwd]').to(:reload).immediately
       end
       it do
         expect(chef_run.ohai('jenkins_reload_passwd')).to do_nothing
