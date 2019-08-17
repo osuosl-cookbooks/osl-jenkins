4ba8b93e685f11b047d780560e8b6e26b91093bd
Khuong Luu <6059347+khashf@users.noreply.github.com>
2019-08-16 17:33:45 -0700

    Fix string concat error & reverse usage of http resource
    Because of UTF-8 issue

--- test/integration/site_pr_builder/inspec/site_pr_builder_spec.rb
+++ test/integration/site_pr_builder/inspec/site_pr_builder_spec.rb
@@ -1,13 +1,9 @@
 sites = %w(beaver-barcamp-pelican_pr_builder osuosl-pelican_pr_builder
            wiki_pr_builder docs_pr_builder)
 
-sites.each do |_job|
-  # describe command("curl -k https://127.0.0.1/job/#{job}/ -o /dev/null -v 2>&1") do
-  #   its('stdout') { should match(/X-Jenkins-Session:/) }
-  #   its('exit_status') { should eq 0 }
-  # end
-
-  describe http('https://127.0.0.1/job/#{job}/', enable_remote_worker: true, ssl_verify: false) do
-    its('body') { should match(/X-Jenkins-Session:/) }
+sites.each do |job|
+  describe command("curl -k https://127.0.0.1/job/#{job}/ -o /dev/null -v 2>&1") do
+    its('stdout') { should match(/X-Jenkins-Session:/) }
+    its('exit_status') { should eq 0 }
   end
 end
