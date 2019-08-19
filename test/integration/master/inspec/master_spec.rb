49c637a8dca3f5024b4d367c40dab1b498cc0d66
Khuong Luu <6059347+khashf@users.noreply.github.com>
2019-08-19 12:10:10 -0700

    Remove utf-8 conversion
    >> is clearly in UTF-8 charset but iconv complains it's illegal

--- test/integration/master/inspec/master_spec.rb
+++ test/integration/master/inspec/master_spec.rb
@@ -1,4 +1,3 @@
-describe command('curl -k https://127.0.0.1/credentials/store/system/domain/_/ \
-                 | iconv -f US-ASCII -t UTF-8') do
+describe command('curl -k https://127.0.0.1/credentials/store/system/domain/_/') do
   its('stdout') { should match(/alfred \(Credentials for alfred - created by Chef\)/) }
 end
