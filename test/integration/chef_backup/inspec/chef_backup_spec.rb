4e5db5b9058f10b651182b8a84b95829e7e4ce8d
Khuong Luu <6059347+khashf@users.noreply.github.com>
2019-08-20 15:01:09 -0700

    Fix chef-backup tests

--- test/integration/chef_backup/inspec/chef_backup_spec.rb
+++ test/integration/chef_backup/inspec/chef_backup_spec.rb
@@ -1,11 +1,5 @@
-RSpec.configure do |config|
-  config.before(:all) do
-    config.path = '/opt/chef/embedded/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
-  end
-end
-
-describe package('knife-backup') do
-  its('version') { should eq '0.0.10' }
+describe command('/opt/chef/embedded/bin/gem list') do
+  its('stdout') { should match(%r{knife-backup\s\(0.0.10\)}) }
 end
 
 describe file('/var/chef-backup-for-rdiff') do
