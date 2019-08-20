feaa304099b4c21fd463141b5834d0b04abbf004
Khuong Luu <6059347+khashf@users.noreply.github.com>
2019-08-20 11:47:06 -0700

    Clean up lines

--- test/integration/chef_backup/inspec/chef_backup_spec.rb
+++ test/integration/chef_backup/inspec/chef_backup_spec.rb
@@ -1,7 +1,6 @@
 RSpec.configure do |config|
   config.before(:all) do
-    config.path = '/opt/chef/embedded/bin:/usr/local/sbin:
-                   /usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+    config.path = '/opt/chef/embedded/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
   end
 end
 
