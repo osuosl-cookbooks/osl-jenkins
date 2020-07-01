7b97773f94b7686f61dd8eca0046b508def16e7f
Ryan Zimmerman <zimmerry@oregonstate.edu>
2020-07-01 11:01:04 -0700

    Fixed InSpec for chef-backup

--- test/integration/chef_backup/inspec/chef_backup_spec.rb
+++ test/integration/chef_backup/inspec/chef_backup_spec.rb
@@ -1,4 +1,7 @@
-describe command('/opt/chef/embedded/bin/gem list') do
+chef_installed = inspec.file('opt/chef/bin/chef-client').exist?
+chef = chef_installed ? 'chef' : 'cinc'
+
+describe command("/opt/#{chef}/embedded/bin/gem list") do
   its('stdout') { should match(/knife-backup\s\(0.0.10\)/) }
 end
 
